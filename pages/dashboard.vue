2a37c17358e89907ae9309c448c13c4f455bc74c
Mathis Sergent <mathis@Mathiss-MacBook-Air.local>
2025-04-17 15:53:52 +0200

    Add welcome text

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="flex flex-col gap-6 p-10 min-h-screen bg-gray-100">
-        <div class="w-2/3 mx-auto space-y-6">
-            <h1 class="text-2xl font-bold">Dashboard</h1>
-            <div class="flex flex-col gap-4">
-                <h2 class="text-xl font-bold">Classrooms</h2>
-                <ClassroomsList />
+        <div class="w-2/3 mx-auto space-y-10">
+            <h1 class="text-2xl font-bold text-center">Hey User, Welcome Back ☀️</h1>
+            <div class="space-y-6">
+                <h1 class="text-2xl font-bold">Dashboard</h1>
+                <div class="flex flex-col gap-4">
+                    <h2 class="text-xl font-bold">Classrooms</h2>
+                    <ClassroomsList />
+                </div>
             </div>
         </div>
     </div>
